4c87b7af358e0c69bdaf02713fa3d440eabaddcf
Alex Fang <deeno@qq.com>
2024-08-27 18:37:45 +0800

    chore(font): update font slicer for individual characters by language

--- public-src/font-slice.ts
+++ public-src/font-slice.ts
@@ -2,15 +2,23 @@ import Path from 'node:path';
 import URL from 'node:url';
 import { globbySync } from 'globby';
 import FsExtra from 'fs-extra';
+import SmolToml from 'smol-toml';
 import FontMin from 'fontmin';
 import GulpRename from 'gulp-rename';
 
-const CURFILE = URL.fileURLToPath(import.meta.url);
-const __dirname = Path.resolve(CURFILE, '../../');
+const __filename = URL.fileURLToPath(import.meta.url);
+const __dirname = Path.resolve(__filename, '../../');
 
 const STATIC_CHAR_LIST: string[] = [
     '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|} ',
 ];
+const CLASS_FONT_MAP: Record<string, string[]> = {
+    'font-misans': ['MiSans-Medium', 'MiSans-Semibold'],
+    'font-misans-latin': ['MiSansLatin-Medium', 'MiSansLatin-Semibold'],
+    'font-misans-tc': ['MiSansTC-Medium', 'MiSansTC-Semibold'],
+};
+
+const uniqueArray = <T extends string | number>(arr: T[]): T[] => Array.from(new Set(arr));
 
 const getGlob = (pattern: string, cwd: string = __dirname): string[] => {
     const patternGlob: string = Path.resolve(cwd, pattern).replace(/\\/gui, '/');
@@ -23,34 +31,86 @@ const readFile = (path: string) => {
     return content;
 };
 
+const getFontNamesByClass = (className: string) => CLASS_FONT_MAP[className] ?? [];
+
+interface TranslationData {
+    profile: {
+        font_class: string,
+    },
+}
+
+interface FontTrimConfig {
+    fontClass: string,
+    fontFiles: string[],
+    text: string,
+    supportLangs: string[],
+}
+
 (async function () {
-    // Get translation text
+    // Build font trim config
     let translationPaths = getGlob('./src/locale/translations/**/*.toml');
-    let translationContent = translationPaths
-        .map(path => readFile(path))
-        .join('');
-    let translationFullContent = `${translationContent}${STATIC_CHAR_LIST.join('')}`;
+    let fontTrimConfigMap: Record<string, FontTrimConfig> = translationPaths
+        .reduce((obj, translationPath, _translationPathIdx, _arr): Record<string, FontTrimConfig> => {
+            let translationName = Path.basename(translationPath, '.toml');
+            let translationContent = readFile(translationPath);
+            let translationObj = SmolToml.parse(translationContent) as unknown as TranslationData;
+            let fontClass = translationObj['profile']['font_class'];
+            let fontNames = getFontNamesByClass(fontClass);
+
+            if (fontNames.length <= 0) return obj;
+
+            let fontTrimConfig = obj[fontClass];
+            if (!fontTrimConfig) {
+                fontTrimConfig = {
+                    fontClass,
+                    fontFiles: fontNames,
+                    text: translationContent,
+                    supportLangs: [translationName],
+                };
+            } else {
+                fontTrimConfig = {
+                    fontClass,
+                    fontFiles: uniqueArray([...fontTrimConfig.fontFiles, ...fontNames]),
+                    text: `${fontTrimConfig.text}${translationContent}`,
+                    supportLangs: [...fontTrimConfig.supportLangs, translationName],
+                };
+            }
+            obj[fontClass] = fontTrimConfig;
+
+            return obj;
+        }, {} as Record<string, FontTrimConfig>);
+    let fontTrimConfigList: FontTrimConfig[] = Object.values(fontTrimConfigMap);
 
     // Trim font
-    let fontPaths = getGlob('./public-src/**/*.ttf');
     let fontTargetBase: string = Path.resolve(__dirname, './public/fonts/');
-    for (let fontPath of fontPaths) {
-        const fontName: string = Path.basename(fontPath, '.ttf');
-        const fontCompressedName: string = `${fontName}-compressed.woff2`;
+    for (let trimConfig of fontTrimConfigList) {
+        const trimFontFiles: string[] = trimConfig.fontFiles ?? [];
+        const trimText: string = trimConfig.text ?? '';
+        const trimTextFull: string = `${trimText}${STATIC_CHAR_LIST.join('')}`;
+        const trimSupportLangs: string[] = trimConfig.supportLangs ?? [];
 
-        new FontMin.default()
-            .src(fontPath)
-            .dest(fontTargetBase)
-            .use(FontMin.default.glyph({
-                text: translationFullContent,
-            }))
-            .use(FontMin.default.ttf2woff2())
-            .use(GulpRename(fontCompressedName))
-            .run((e?: Error) => {
-                if (e) {
-                    console.error(e.message);
-                }
-                console.log(`Sliced font: ${fontName}.ttf → ${fontCompressedName}`);
-            });
+        for (let fontFile of trimFontFiles) {
+            let fontPaths = getGlob(`./public-src/**/${fontFile}.ttf`);
+            for (let fontPath of fontPaths) {
+                const fontName: string = Path.basename(fontPath, '.ttf');
+                const fontCompressedName: string = `${fontName}-compressed.woff2`;
+
+                new FontMin.default()
+                    .src(fontPath)
+                    .dest(fontTargetBase)
+                    .use(FontMin.default.glyph({
+                        text: trimTextFull,
+                    }))
+                    .use(FontMin.default.ttf2woff2())
+                    .use(GulpRename(fontCompressedName))
+                    .run((e?: Error) => {
+                        if (e) {
+                            console.error(e.message);
+                        }
+                        console.log(`○ Sliced font: ${fontName}.ttf → ${fontCompressedName}`);
+                        console.log(`→   Supported langs: ${trimSupportLangs.join(' ')}`);
+                    });
+            }
+        }
     }
 })();
