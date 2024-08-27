329ee2525c9fb110a3a16ce8e58949242f1287e1
Alex Fang <deeno@qq.com>
2024-08-27 18:45:42 +0800

    refactor(font-slice): restructure font slicer script to a clear structure

--- public-src/font-slice.ts
+++ public-src/font-slice.ts
@@ -9,6 +9,19 @@ import GulpRename from 'gulp-rename';
 const __filename = URL.fileURLToPath(import.meta.url);
 const __dirname = Path.resolve(__filename, '../../');
 
+interface ProfileData {
+    theme: {
+        font_class: string,
+    },
+}
+
+interface FontTrimConfig {
+    fontClass: string,
+    fontFiles: string[],
+    text: string,
+    supportLangs: string[],
+}
+
 const STATIC_CHAR_LIST: string[] = [
     '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|} ',
 ];
@@ -26,93 +39,91 @@ const getGlob = (pattern: string, cwd: string = __dirname): string[] => {
     return matchPaths;
 }
 
-const readFile = (path: string) => {
+const readFile = (path: string): string => {
     const content: string = FsExtra.readFileSync(path, { encoding: 'utf-8' }) || '';
     return content;
 };
 
-const getFontNamesByClass = (className: string) => CLASS_FONT_MAP[className] ?? [];
+const getFontNamesByClass = (className: string): string[] => CLASS_FONT_MAP[className] ?? [];
 
-interface ProfileData {
-    theme: {
-        font_class: string,
-    },
-}
+const accumulateTrimConfig = (
+    obj: Record<string, FontTrimConfig>,
+    profilePath: string,
+    _profilePathIdx: number,
+    _arr: string[]
+): Record<string, FontTrimConfig> => {
+    let profileName = Path.basename(profilePath, '.toml');
+    let profileContent = readFile(profilePath);
+    let profileObj = SmolToml.parse(profileContent) as unknown as ProfileData;
+    let translationPath = Path.resolve(__dirname, `./src/languages/translations/${profileName}.toml`)
+    let translationContent = readFile(translationPath);
+    let fontClass = profileObj['theme']['font_class'];
+    let fontNames = getFontNamesByClass(fontClass);
 
-interface FontTrimConfig {
-    fontClass: string,
-    fontFiles: string[],
-    text: string,
-    supportLangs: string[],
-}
+    if (fontNames.length <= 0) return obj;
+
+    let fontTrimConfig = obj[fontClass];
+    if (!fontTrimConfig) {
+        fontTrimConfig = {
+            fontClass,
+            fontFiles: fontNames,
+            text: translationContent,
+            supportLangs: [profileName],
+        };
+    } else {
+        fontTrimConfig = {
+            fontClass,
+            fontFiles: uniqueArray([...fontTrimConfig.fontFiles, ...fontNames]),
+            text: `${fontTrimConfig.text}${translationContent}`,
+            supportLangs: [...fontTrimConfig.supportLangs, profileName],
+        };
+    }
+    obj[fontClass] = fontTrimConfig;
+
+    return obj;
+};
+
+const trimFont = (compressBase: string, config: FontTrimConfig) => {
+    const trimFontFiles: string[] = config.fontFiles ?? [];
+    const trimText: string = config.text ?? '';
+    const trimTextFull: string = `${trimText}${STATIC_CHAR_LIST.join('')}`;
+    const trimSupportLangs: string[] = config.supportLangs ?? [];
+
+    for (let fontFile of trimFontFiles) {
+        let fontPaths = getGlob(`./public-src/**/${fontFile}.ttf`);
+        for (let fontPath of fontPaths) {
+            const fontName: string = Path.basename(fontPath, '.ttf');
+            const fontCompressedName: string = `${fontName}-compressed.woff2`;
+
+            new FontMin.default()
+                .src(fontPath)
+                .dest(compressBase)
+                .use(FontMin.default.glyph({
+                    text: trimTextFull,
+                }))
+                .use(FontMin.default.ttf2woff2())
+                .use(GulpRename(fontCompressedName))
+                .run((e?: Error) => {
+                    if (e) {
+                        console.error(e.message);
+                    }
+                    console.log(`○ Sliced font: ${fontName}.ttf → ${fontCompressedName}`);
+                    console.log(`→   Supported langs: ${trimSupportLangs.join(' ')}`);
+                });
+        }
+    }
+};
 
 (async function () {
     // Build font trim config
     let profilePaths = getGlob('./src/languages/profile/*.toml');
-    let fontTrimConfigMap: Record<string, FontTrimConfig> = profilePaths
-        .reduce((obj, profilePath, _profilePathIdx, _arr): Record<string, FontTrimConfig> => {
-            let profileName = Path.basename(profilePath, '.toml');
-            let profileContent = readFile(profilePath);
-            let profileObj = SmolToml.parse(profileContent) as unknown as ProfileData;
-            let translationPath = Path.resolve(__dirname, `./src/languages/translations/${profileName}.toml`)
-            let translationContent = readFile(translationPath);
-            let fontClass = profileObj['theme']['font_class'];
-            let fontNames = getFontNamesByClass(fontClass);
-
-            if (fontNames.length <= 0) return obj;
-
-            let fontTrimConfig = obj[fontClass];
-            if (!fontTrimConfig) {
-                fontTrimConfig = {
-                    fontClass,
-                    fontFiles: fontNames,
-                    text: translationContent,
-                    supportLangs: [profileName],
-                };
-            } else {
-                fontTrimConfig = {
-                    fontClass,
-                    fontFiles: uniqueArray([...fontTrimConfig.fontFiles, ...fontNames]),
-                    text: `${fontTrimConfig.text}${translationContent}`,
-                    supportLangs: [...fontTrimConfig.supportLangs, profileName],
-                };
-            }
-            obj[fontClass] = fontTrimConfig;
-
-            return obj;
-        }, {} as Record<string, FontTrimConfig>);
+    let fontTrimConfigMap: Record<string, FontTrimConfig> = {};
+    fontTrimConfigMap = profilePaths.reduce(accumulateTrimConfig, fontTrimConfigMap);
     let fontTrimConfigList: FontTrimConfig[] = Object.values(fontTrimConfigMap);
 
     // Trim font
     let fontTargetBase: string = Path.resolve(__dirname, './public/fonts/');
     for (let trimConfig of fontTrimConfigList) {
-        const trimFontFiles: string[] = trimConfig.fontFiles ?? [];
-        const trimText: string = trimConfig.text ?? '';
-        const trimTextFull: string = `${trimText}${STATIC_CHAR_LIST.join('')}`;
-        const trimSupportLangs: string[] = trimConfig.supportLangs ?? [];
-
-        for (let fontFile of trimFontFiles) {
-            let fontPaths = getGlob(`./public-src/**/${fontFile}.ttf`);
-            for (let fontPath of fontPaths) {
-                const fontName: string = Path.basename(fontPath, '.ttf');
-                const fontCompressedName: string = `${fontName}-compressed.woff2`;
-
-                new FontMin.default()
-                    .src(fontPath)
-                    .dest(fontTargetBase)
-                    .use(FontMin.default.glyph({
-                        text: trimTextFull,
-                    }))
-                    .use(FontMin.default.ttf2woff2())
-                    .use(GulpRename(fontCompressedName))
-                    .run((e?: Error) => {
-                        if (e) {
-                            console.error(e.message);
-                        }
-                        console.log(`○ Sliced font: ${fontName}.ttf → ${fontCompressedName}`);
-                        console.log(`→   Supported langs: ${trimSupportLangs.join(' ')}`);
-                    });
-            }
-        }
+        trimFont(fontTargetBase, trimConfig);
     }
 })();
